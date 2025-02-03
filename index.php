091c3b14853b92f326a85aa9fe939706761cb94a
Kaurmanmeet04 <157570158+Kaurmanmeet04@users.noreply.github.com>
2025-02-03 15:38:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+<main>
+    <h1>Welcome to my first PHP Website</h1><br>
+    <img src="images/IMG_2877.jpeg" alt="Manmeet Kaur" width="300" height="300">
+    <h3><b>-Manmeet Kaur</b></h3><br><br>
+    <p class="para">This website was created as part of a PHP assignment. It's a learning project designed to explore web development concepts, including PHP, HTML, and more. While it may not be a full-fledged site, it showcases my progress in coding. Thanks for stopping by!</p>
+</main>
+<?php include 'footer.php'; ?>
