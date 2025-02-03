e3ab17be39cd0a6cc227a5f4c32eff0420602e5e
Kaurmanmeet04 <157570158+Kaurmanmeet04@users.noreply.github.com>
2025-02-03 15:37:09 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+ 
+<main>
+    <h1>About Us</h1><br><br>
+    <h2>Our Team</h2><br>
+    <ul>
+        <li>
+            <img src="images/IMG_2877.jpeg" alt="Manmeet Kaur" width="300" height="300">
+            <h3>Manmeet Kaur - Designer</h3><br>
+            <p class= "para2">Manmeet is the creative mind behind the website's look and feel. She focuses on crafting visually appealing designs, intuitive layouts, and an engaging user interface. By combining aesthetics with usability, she ensures that visitors have a smooth and enjoyable browsing experience. Her design choices enhance the website's identity and make it both functional and attractive.</p>
+        </li>
+        <li>
+            <img src="images/simranjot.jpg"alt="Simran" width="200" height="300">
+            <h3>Simran - Developer</h3><br>
+            <p class= "para2">Simran is the backbone of the website's functionality, handling the coding and technical aspects of the project. With expertise in PHP, HTML, CSS, and JavaScript, she ensures that the website runs smoothly, loads efficiently, and delivers a seamless user experience. From backend logic to front-end interactions, Simran brings the site to life with her development skills.</p>
+        </li>
+        <li>
+            <img src="images/PHOTO-2025-01-27-19-13-47.jpg"alt="Neha" width="200" height="300">
+            <h3>Neha - Product Manager</h3><br>
+            <p class= "para2">Neha plays a crucial role in overseeing the project's direction and execution. As the product manager, she coordinates between the development and design teams, ensuring that the website meets its goals and aligns with user needs. She manages timelines, sets priorities, and ensures that the final product is polished, user-friendly, and effective. Her leadership keeps the project on track from start to finish.</p>
+        </li>
+    </ul>
+</main>
+<?php include 'footer.php'; ?>
